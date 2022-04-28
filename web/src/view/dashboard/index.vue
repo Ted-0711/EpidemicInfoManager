5f217cb26f0f3043792687880d1d66b0a0d50cc5
Ted_Xu <1853769@tongji.edu.cn>
2022-04-28 14:10:10 +0800

    Update dashboard

--- web/src/view/dashboard/index.vue
+++ web/src/view/dashboard/index.vue
@@ -4,7 +4,7 @@
       <div class="gva-card gva-top-card">
         <div class="gva-top-card-left">
           <div class="gva-top-card-left-title">早安，{{ userStore.userInfo.nickName }}，请开始一天的工作吧</div>
-          <div class="gva-top-card-left-dot">上海市嘉定区，今日{{ this.weather['weather'] }}，气温{{ this.weather['temp_low'] }}℃ - {{ this.weather['temp_high'] }}℃，湿度{{ this.weather['humidity'] }}，{{ this.weather['wind'] }} {{ this.weather['winp'] }}。</div>
+          <div class="gva-top-card-left-dot">上海市嘉定区，今日{{ this.weather['weather'] }}，气温{{ this.weather['temp_low'] }}℃ - {{ this.weather['temp_high'] }}℃，当前湿度{{ this.weather['humidity'] }}，{{ this.weather['wind'] }} {{ this.weather['winp'] }}。</div>
           <div class="gva-top-card-left-rows">
             <el-row v-auth="888">
               <el-col :span="8" :xs="24" :sm="8">
@@ -35,20 +35,20 @@
           </div>
           <div>
             <div class="gva-top-card-left-item">
-              使用教学：
+              仓库地址：
               <a
                 style="color:#409EFF"
                 target="view_window"
-                href="https://www.bilibili.com/video/BV1Rg411u7xH/"
-              >https://www.bilibili.com/video/BV1Rg411u7xH</a>
+                href="https://github.com/Ted-0711/EpidemicInfoManager/"
+              >https://github.com/Ted-0711/EpidemicInfoManager</a>
             </div>
             <div class="gva-top-card-left-item">
-              插件仓库：
+              个人主页：
               <a
                 style="color:#409EFF"
                 target="view_window"
-                href="https://github.com/flipped-aurora/gva-plugins"
-              >https://github.com/flipped-aurora/gva-plugins</a>
+                href="http://www.tedxu.xyz/"
+              >http://www.tedxu.xyz</a>
             </div>
           </div>
         </div>
@@ -64,7 +64,7 @@
         </template>
         <el-row :gutter="20">
           <el-col
-            v-for="(card, key) in toolCards"
+            v-for="(card, key) in this.toolCards"
             :key="key"
             :span="4"
             :xs="8"
@@ -197,13 +197,200 @@ export default {
         windid: "1",
         winp: "2级",
         winpid: "2",
-      }
+      },
+      toolCards: [
+        {
+          label: '用户管理',
+          icon: 'monitor',
+          name: 'user',
+          color: '#ff9c6e',
+          bg: 'rgba(255, 156, 110,.3)'
+        },
+        {
+          label: '角色管理',
+          icon: 'setting',
+          name: 'authority',
+          color: '#69c0ff',
+          bg: 'rgba(105, 192, 255,.3)'
+        },
+        {
+          label: '菜单管理',
+          icon: 'menu',
+          name: 'menu',
+          color: '#b37feb',
+          bg: 'rgba(179, 127, 235,.3)'
+        },
+        {
+          label: '代码生成器',
+          icon: 'cpu',
+          name: 'autoCode',
+          color: '#ffd666',
+          bg: 'rgba(255, 214, 102,.3)'
+        },
+        {
+          label: '表单生成器',
+          icon: 'document-checked',
+          name: 'formCreate',
+          color: '#ff85c0',
+          bg: 'rgba(255, 133, 192,.3)'
+        },
+        {
+          label: '关于我们',
+          icon: 'user',
+          name: 'about',
+          color: '#5cdbd3',
+          bg: 'rgba(92, 219, 211,.3)'
+        }
+      ],
     }
   },
   created() {
     getWeather().then((res) => {
       this.weather = JSON.parse(res['data']['list'])['result']
       console.log(this.weather)
+    })
+    const userStore = useUserStore()
+    userStore.GetUserInfo().then((res) => {
+      let authorityId = res['data']['userInfo']['authority']['authorityId']
+      console.log(authorityId)
+      if (authorityId === '888') {
+        this.toolCards = [
+          {
+            label: '用户管理',
+            icon: 'monitor',
+            name: 'user',
+            color: '#ff9c6e',
+            bg: 'rgba(255, 156, 110,.3)'
+          },
+          {
+            label: '角色管理',
+            icon: 'setting',
+            name: 'authority',
+            color: '#69c0ff',
+            bg: 'rgba(105, 192, 255,.3)'
+          },
+          {
+            label: '菜单管理',
+            icon: 'menu',
+            name: 'menu',
+            color: '#b37feb',
+            bg: 'rgba(179, 127, 235,.3)'
+          },
+          {
+            label: '代码生成器',
+            icon: 'cpu',
+            name: 'autoCode',
+            color: '#ffd666',
+            bg: 'rgba(255, 214, 102,.3)'
+          },
+          {
+            label: '表单生成器',
+            icon: 'document-checked',
+            name: 'formCreate',
+            color: '#ff85c0',
+            bg: 'rgba(255, 133, 192,.3)'
+          },
+          {
+            label: '关于我们',
+            icon: 'user',
+            name: 'about',
+            color: '#5cdbd3',
+            bg: 'rgba(92, 219, 211,.3)'
+          }
+        ]
+      }
+      else if (authorityId === '9990') {
+        this.toolCards = [
+          {
+            label: '每日打卡',
+            icon: 'alarm-clock',
+            name: 'clock_in_report',
+            color: '#ff9c6e',
+            bg: 'rgba(255, 156, 110,.3)'
+          },
+          {
+            label: '疫苗接种',
+            icon: 'arrow-down-bold',
+            name: 'vaccine_report',
+            color: '#69c0ff',
+            bg: 'rgba(105, 192, 255,.3)'
+          },
+          {
+            label: '核酸检测',
+            icon: 'warning-filled',
+            name: 'test_swab_report',
+            color: '#b37feb',
+            bg: 'rgba(179, 127, 235,.3)'
+          },
+          {
+            label: '隔离记录',
+            icon: 'key',
+            name: 'quarantine_report',
+            color: '#ffd666',
+            bg: 'rgba(255, 214, 102,.3)'
+          },
+          {
+            label: '人口流动',
+            icon: 'bicycle',
+            name: 'migration_report',
+            color: '#ff85c0',
+            bg: 'rgba(255, 133, 192,.3)'
+          },
+          // {
+          //   label: '问卷填写',
+          //   icon: 'user',
+          //   name: 'about',
+          //   color: '#5cdbd3',
+          //   bg: 'rgba(92, 219, 211,.3)'
+          // }
+        ]
+      }
+      else if (authorityId === '9991') {
+        this.toolCards = [
+          {
+            label: '打卡记录',
+            icon: 'alarm-clock',
+            name: 'clock_in_query',
+            color: '#ff9c6e',
+            bg: 'rgba(255, 156, 110,.3)'
+          },
+          {
+            label: '疫苗接种',
+            icon: 'arrow-down-bold',
+            name: 'vaccine_query',
+            color: '#69c0ff',
+            bg: 'rgba(105, 192, 255,.3)'
+          },
+          {
+            label: '核酸检测',
+            icon: 'warning-filled',
+            name: 'test_swab_query',
+            color: '#b37feb',
+            bg: 'rgba(179, 127, 235,.3)'
+          },
+          {
+            label: '隔离记录',
+            icon: 'key',
+            name: 'quarantine_query',
+            color: '#ffd666',
+            bg: 'rgba(255, 214, 102,.3)'
+          },
+          {
+            label: '人口流动',
+            icon: 'bicycle',
+            name: 'migration_query',
+            color: '#ff85c0',
+            bg: 'rgba(255, 133, 192,.3)'
+          },
+          // {
+          //   label: '问卷填写',
+          //   icon: 'user',
+          //   name: 'about',
+          //   color: '#5cdbd3',
+          //   bg: 'rgba(92, 219, 211,.3)'
+          // }
+        ]
+      }
     })
   }
 }
